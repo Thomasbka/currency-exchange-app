80a9b3806200e2317b3c3b8ca026c2cdcbb04608
Thomasbka <tolle_bka@hotmail.com>
2024-06-07 13:28:02 +0200

    Added a button that when clicked will show a graph with historic data of the selected currencypair. Also improved the navbar so that it is more responsive and changes depending on screen size.

--- src/Converter.js
+++ src/Converter.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './Converter.css';
 import fx from 'money';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 class Converter extends Component {
   constructor(props) {
@@ -13,7 +15,11 @@ class Converter extends Component {
       toCurrency: 'EUR',
       result: null,
       rates: {},
+      showGraphButton: false,
+      showGraph: false,
+      chartData: null,
     };
+    this.chartRef = React.createRef();
   }
 
   componentDidMount() {
@@ -29,7 +35,7 @@ class Converter extends Component {
   handleConvert = () => {
     const { amount, fromCurrency, toCurrency } = this.state;
     const convertedAmount = fx(amount).from(fromCurrency).to(toCurrency);
-    this.setState({ result: convertedAmount });
+    this.setState({ result: convertedAmount, showGraphButton: true });
   };
 
   handleSwitch = () => {
@@ -51,13 +57,92 @@ class Converter extends Component {
     this.setState({ toCurrency: e.target.value });
   };
 
+  getHistoricalRates = (base, quote) => {
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date(new Date().getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+
+    fetch(`https://api.frankfurter.app/${startDate}..${endDate}?from=${base}&to=${quote}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        const chartLabels = Object.keys(data.rates);
+        const chartData = Object.values(data.rates).map(rate => rate[quote]);
+        const chartLabel = `${base}/${quote}`;
+        this.buildChart(chartLabels, chartData, chartLabel);
+      })
+      .catch(error => console.error(error.message));
+  }
+
+  buildChart = (labels, data, label) => {
+    const chartRef = this.chartRef.current.getContext("2d");
+  
+    if (typeof this.chart !== "undefined") {
+      this.chart.destroy();
+    }
+  
+    this.chart = new Chart(chartRef, {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: label,
+            data,
+            fill: false,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+            tension: 0.1,
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              color: 'white'
+            }
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              color: 'rgba(255, 255, 255, 0.1)',
+            },
+            ticks: {
+              color: 'white'
+            }
+          },
+          y: {
+            grid: {
+              color: 'rgba(255, 255, 255, 0.1)',
+            },
+            ticks: {
+              color: 'white'
+            }
+          }
+        }
+      }
+    });
+  }
+  
+
+  handleGetGraph = () => {
+    const { fromCurrency, toCurrency } = this.state;
+    this.getHistoricalRates(fromCurrency, toCurrency);
+    this.setState({ showGraph: true });
+  }
+
   render() {
-    const { amount, fromCurrency, toCurrency, result, rates } = this.state;
+    const { amount, fromCurrency, toCurrency, result, rates, showGraphButton, showGraph } = this.state;
 
     return (
       <div className="converter">
-        <div className="text-uppercase text-center">
-          <h2 className="headline">Currency Converter</h2>
+        <div className="headline">
+          <h2>Currency Converter</h2>
         </div>
         <div className="container">
           <div className="form-row">
@@ -97,13 +182,23 @@ class Converter extends Component {
           <button className="convert-button" onClick={this.handleConvert}>
             Convert
           </button>
-          <div className="result">
-            {result && (
+          {result && (
+            <div className="result">
               <p>
                 {amount} {fromCurrency} = {result.toFixed(4)} {toCurrency}
               </p>
-            )}
-          </div>
+              {showGraphButton && (
+                <button className="get-graph-button" onClick={this.handleGetGraph}>
+                  Get Graph
+                </button>
+              )}
+            </div>
+          )}
+          {showGraph && (
+            <div className="chart-container">
+              <canvas ref={this.chartRef} />
+            </div>
+          )}
         </div>
       </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Navbar.css';
 
 class Navbar extends Component {
   handleHowToClick = () => {
@@ -18,27 +19,27 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar bg-body-tertiary fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Currency Converter</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas offcanvas-end custom-offcanvas" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Currency Converter</h5>
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-items">
+                <li className="nav-item">
                   <Link className="nav-link" aria-current="page" to="/">Home</Link>
                 </li>
-                <li classNAme="nav-items">
+                <li className="nav-item">
                   <Link className="nav-link" aria-current="page" to="/rates">Table of Rates</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#" onClick={this.handleHowToClick}>How To</a>
+                  <a className="nav-link" href="#" onClick={this.handleHowToClick}>How To</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#" onClick={this.handleAboutClick}>About</a>
